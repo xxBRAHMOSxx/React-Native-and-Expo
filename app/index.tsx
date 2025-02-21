77a446e007ed891335ceb9ef531acb11611e41b1
Devansh Kumar <devanshkumar6298@gmail.com>
2025-02-21 17:15:54 +0530

    refactor index component; update structure and styling

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,12 @@
-import { Text, View } from "react-native";
+import { View, Text } from 'react-native'
+import React from 'react'
 
-export default function Index() {
+const index = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={{backgroundColor: 'red', flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>index</Text>
     </View>
-  );
+  )
 }
+
+export default index
